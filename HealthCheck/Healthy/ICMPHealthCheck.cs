cd69f9250a4c5bf074daa426e5a3860d8fdb839c
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-03-17 10:39:20 +0600

    Add project files.

---
+++ HealthCheck/Healthy/ICMPHealthCheck.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheck.Healthy
+{
+    public class ICMPHealthCheck : IHealthCheck
+    {
+        private readonly string Host = "www.does-not-exist";
+        private readonly int HealthyRoundtripTime = 300;
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
