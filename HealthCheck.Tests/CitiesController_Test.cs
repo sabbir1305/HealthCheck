514bb6473f8ed62bca885d597d423959c10e7b59
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-03-28 12:04:10 +0600

    City TEst

--- HealthCheck.Tests/CitiesController_Test.cs
+++ HealthCheck.Tests/CitiesController_Test.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HealthCheck.Controllers.WorldCity;
+using HealthCheck.Data;
+using HealthCheck.Data.Models.WorldCities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,10 +17,37 @@ namespace HealthCheck.Tests
         public async void GetCiyShouldReturnCity()
         {
             //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "WorldCitiesDb").Options;
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Add(new City()
+                {
+                    Id = 1,
+                    CountryId = 1,
+                    Lat = 1,
+                    Lon = 1,
+                    Name = "Test City"
+                });
 
+                context.SaveChanges();
+            }
+
+            City city_existing = null;
+            City city_notExisting = null;
             //Act
 
+            using (var context = new ApplicationDbContext(options))
+            {
+                var controller = new CitiesController(context);
+                city_existing = (await controller.GetCity(1)).Value;
+                city_notExisting  = (await controller.GetCity(10)).Value;
+            }
+
             //Assert
+
+            Assert.NotNull(city_existing);
+            Assert.Null(city_notExisting);
         }
     }
 }
